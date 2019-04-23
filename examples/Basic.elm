94b31793e24836b39af80779106d4e27d023fb4b
Matías Lang <yo@matiaslang.me>
2019-04-23 20:47:38 -0300

    Refactor basic example
    Get rid of let..in blocks to make the code cleaner

--- examples/Basic.elm
+++ examples/Basic.elm
@@ -8,21 +8,24 @@ import Set
 
 notes : Set.Set Piano.Note
 notes =
-    let
-        -- D, F#, A
-        dMajorKeys =
-            [ 2, 6, 9 ]
-
-        -- Show the notes D, F#, A in all the octaves
-        shouldDisplayNote : Piano.Note -> Bool
-        shouldDisplayNote note =
-            List.member
-                (remainderBy 12 note)
-                dMajorKeys
-    in
     Piano.allNotes
         |> List.filter shouldDisplayNote
         |> Set.fromList
+
+
+{-| Show the notes D, F#, A in all the octaves
+-}
+shouldDisplayNote : Piano.Note -> Bool
+shouldDisplayNote note =
+    List.member
+        (remainderBy 12 note)
+        dMajorKeys
+
+
+{-| D, F#, A
+-}
+dMajorKeys =
+    [ 2, 6, 9 ]
 
 
 
@@ -31,12 +34,12 @@ notes =
 
 main : Html Never
 main =
-    let
-        pianoConfig : Piano.Config
-        pianoConfig =
-            Piano.makeConfig Piano.keyboard88Keys
-                |> Piano.colorAllPressedKeys
-                    (Color.toRgb Color.lightOrange)
-                    (Color.toRgb Color.darkOrange)
-    in
     Piano.viewStatic pianoConfig notes
+
+
+pianoConfig : Piano.Config
+pianoConfig =
+    Piano.makeConfig Piano.keyboard88Keys
+        |> Piano.colorAllPressedKeys
+            (Color.toRgb Color.lightOrange)
+            (Color.toRgb Color.darkOrange)
